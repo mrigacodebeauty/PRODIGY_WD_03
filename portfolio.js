66acd935d3bda44a41f58e9fa3191cd969941274
mrigacodebeauty <146204868+mrigacodebeauty@users.noreply.github.com>
2024-02-24 20:49:28 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function openNav1(){
+    document.getElementById("mysidebar").style.width="450px";
+    document.getElementById("main").style.marginLeft="450px";
+}
+function closeNav1(){
+    document.getElementById("mysidebar").style.width="0";
+    document.getElementById("main").style.marginLeft="0";
+}
